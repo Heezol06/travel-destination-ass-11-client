d59c405def19edcc3750daff167b50ab7c27ef24
Heezol06 <shahriarrahmanheezol@gmail.com>
2021-10-29 17:03:09 +0600

    first commit

---
+++ src/component/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css'
+
+
+const Banner = () => {
+    return (
+        <div className="banner" style={{backgroundImage:""}}>
+            <h2 className="banner-text font-monospace fs-1 fw-light">Explore the world</h2>
+        </div>
+    );
+};
+
+export default Banner;
